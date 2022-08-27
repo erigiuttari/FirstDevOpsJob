071f7f3d3da97b7b280e8a5c5d53de8bbf93af05
erigiuttari <58574280+erigiuttari@users.noreply.github.com>
2022-08-27 17:55:02 -0500

    Update TestController.java

--- src/main/java/aforo255/com/controller/TestController.java
+++ src/main/java/aforo255/com/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
 	@GetMapping("/test")
 	public String TestFirstJob() {
-		return "Welcome to MasterDevops |Tomcat v1346 | Ansible , AFORO255";
+		return "Welcome to MasterDevops |Tomcat v12345 | Ansible , AFORO255 | Erika Giuttari";
 	}
 }
